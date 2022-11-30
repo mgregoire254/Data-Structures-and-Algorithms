8307809d7c04b9f07d6b2bbc55137ffe0f67dacc
mgregoire254 <mgregoire1@me.com>
2022-11-30 14:54:16 -0600

    create stack and push items to it.

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class Main{
+    
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<String>();
+
+
+        stack.push("Minecraft");
+        stack.push("Skyrim");
+        stack.push("DooM");
+        stack.push("Borderlands");
+        stack.push("FFVII");
+        
+        System.out.println(stack);
+
+
+
+
+
+    
+    }
+
+}
