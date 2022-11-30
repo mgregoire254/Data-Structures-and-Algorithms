d1199fbabafc2597ba7fd0abe5587e62a624821d
mgregoire254 <mgregoire1@me.com>
2022-11-30 16:05:55 -0600

    add folder for Queue practice. set up main file

---
+++ Queues/Main.java
@@ -0,0 +1,12 @@
+package Queues;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Main {
+    // Queues are First in First Out data structure
+    public static void main(String[] args) {
+
+        
+
+    }
+}
