4186b0c6bd29d2ec3c7682a8efa5a76f27d68b93
mgregoire254 <mgregoire1@me.com>
2022-11-30 22:19:28 -0600

    add queues and priority queues

---
+++ Priority_Queues/Main.java
@@ -0,0 +1,43 @@
+package Priority_Queues;
+import java.util.Queue;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    // Priority queues are a data structure that has the highest priority items first
+    public static void main(String[] args) {
+
+        Queue<Double> grades = new PriorityQueue<>();
+
+        grades.offer(3.0);
+        grades.offer(2.5);
+        grades.offer(4.0);
+        grades.offer(1.5);
+        grades.offer(2.0);
+
+        while(!grades.isEmpty()) {
+            System.out.println(grades.poll());
+
+        }
+
+        
+        Queue<Double> gradesReversed = new PriorityQueue<>(Collections.reverseOrder());
+
+        gradesReversed.offer(3.0);
+        gradesReversed.offer(2.5);
+        gradesReversed.offer(4.0);
+        gradesReversed.offer(1.5);
+        gradesReversed.offer(2.0);
+
+        while (!gradesReversed.isEmpty()) {
+            System.out.println(gradesReversed.poll());
+            
+        }
+
+
+
+    }
+    
+
+}
--- Queues/Main.java
+++ Queues/Main.java
@@ -3,7 +3,7 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 public class Main {
-    // Queues are First in First Out data structure
+    // Queues are a First in First Out data structure
     public static void main(String[] args) {
 
         //create a queue named queue. Cannot create instance of Queue so must use LinkedList
@@ -14,6 +14,9 @@ public class Main {
         queue.offer("Chad");
         queue.offer("Steve");
         queue.offer("Harold");
+        queue.offer("Bill");
+        queue.offer("Bob");
+        queue.offer("Tom");
 
         // print item at head of queue
         System.out.println(queue.peek());
@@ -26,6 +29,21 @@ public class Main {
 
         //print all remaining items in the queue
         System.out.println(queue);
+
+        queue.poll();
+        queue.poll();
+
+        System.out.println(queue);
+
+        //print boolean value of whether queue is empty or not
+        System.out.println(queue.isEmpty());
+
+        //print size of the queue
+        System.out.println(queue.size());
+
+        //print boolean value for whether or not the queue contains the value "Tom"
+        System.out.println(queue.contains("Tom"));
+        
 
     }
 }
