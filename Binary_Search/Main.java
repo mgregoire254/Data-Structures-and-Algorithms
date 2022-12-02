fe5deb0283cb0bd749f3b644dbf855111e5dc893
mgregoire254 <mgregoire1@me.com>
2022-12-01 23:54:13 -0600

    write binary search method, change searches to it

--- Binary_Search/Main.java
+++ Binary_Search/Main.java
@@ -12,7 +12,7 @@ public class Main {
             array[i] = i;
         }
 
-        int index = Arrays.binarySearch(array, target);
+        int index = binarySearch(array, target);
 
         if(index == -1) {
             System.out.println("Element " + target + " was not found");
@@ -28,7 +28,7 @@ public class Main {
             array2[i] = i;
         }
 
-        int index2 = Arrays.binarySearch(array2, target2);
+        int index2 = binarySearch(array2, target2);
 
         if(index2 == -1) {
             System.out.println("Element " + target2 + " was not found");
@@ -36,5 +36,25 @@ public class Main {
         else {
             System.out.println("Element " + target2 + " found at index: " + index2);
         }
+    }
+
+    private static int binarySearch(int[] array, int target) {
+
+        int low = 0;
+        int high = array.length - 1;
+
+        while(low <= high) {
+
+            int middle = low + (high - low / 2);
+            int value = array[middle];
+
+            System.out.println("middle: " + value);
+
+            if(value < target) low = middle + 1;
+
+            else if(value > target) high = middle - 1;
+            else return middle;
+        }
+        return -1;
     }
 }
